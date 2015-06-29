976ca4df0b0b6fb93ac5fd804e3920ebab017511
井上裕太 <inoue@alfasystem.co.jp>
2015-06-29 17:40:37 +0900

    成果物

--- comment.py
+++ comment.py
@@ -6,31 +6,81 @@ import cgitb
 import MySQLdb
 import urllib
 from string import Template
+import datetime
 cgitb.enable()
 
-def insert_comment():
-    data = cgi.FieldStorage()
-    name = data.getvalue("name","none")
-    date = data.getvalue("date","none")
-    comment = data.getvalue("comment","none")
-
-    connector = MySQLdb.connect(host = "localhost", db = test, user = "inoue", passwd = "inoPWit", charset $
+def show_comment(thread_id):
+    connector = MySQLdb.connect(host = "localhost", db = "test", user = "inoue", passwd = "inoPWit", charset = "utf8")
     cursor = connector.cursor()
 
-    cursor.execute("select thread_id from threads order by thread_id desc;")
+    cursor.execute(select_id.substitute(thread_id = thread_id))
     result = cursor.fetchall()
-    thread_id = result[0]
 
-    cursor.execute("select comment_id from comments order by comment_id desc;")
-    result = cursor.fetchall()
-    comment_id = result[0]
+    i = 0
+    for row in result:
+        i += 1
+        print comments.substitute(number = i, name = row[2].encode("utf-8"), date = str(row[3]), comment = row[4].encode("utf-8"))
 
-    sql = "insert into comments values(" + str(thread_id) + ", " + str(comment_id) + ", " +  name + ", " + $
+def insert_comment(thread_id, name, comment):
+    connector = MySQLdb.connect(host = "localhost", db = "test", user = "inoue", passwd = "inoPWit", charset = "utf8")
+    cursor = connector.cursor()
+
+    #cursor.execute("select thread_id  from test.comments order by thread_id desc;")
+    #result = cursor.fetchone()
+    #if result == None:
+    #    thread_id = 1
+    #else:
+    #    thread_id = result[0]
+
+    insert_text = Template("""select comment_id from test.comments order by comment_id desc;""")
+    cursor.execute(insert_text.substitute())
+    result = cursor.fetchone()
+    if result == None:
+        comment_id = 1
+    else:
+        comment_id = result[0] + 1
+
+    d = datetime.datetime.today()
+    date = str(d.year) + "/" + str(d.month) + "/" + str(d.day) + "/" + str(d.hour) + "/" + str(d.minute) + "/" + str(d.second)
+    
+#    sql = "insert into test.comments values(\"1\", \"1\", \"inoue\", \"2015/6/26/10:20\", \"スレッドを作成しました。\")"
+sql1 = "insert into test.comments values(\"" + str(comment_id) + "\", \"" + str(thread_id) + "\", \"" + name + "\",\"" + date + "\", \"" + comment +"\", \"false \")"
+    cursor.execute(sql1)
+
+    insert_text = Template("""select comment_number from test.threads where thread_id = " + str(thread_id) + ";""")
+    cursor.execute(insert_text.substitute())
+    result = cursor.fetchone()
+    if result == None:
+        comment_number = 1
+    else:
+        comment_number = result[0] + 1
+
+    sql2 = "update test.threads set comment_number = " + str(comment_number)
+    sql3 = "update test.threads set update_day = " + date
+    cursor.execute(sql2)
+    cursor.execute(sql3)
+
+    connector.commit()
+
+    cursor.close()
+    connector.close()
 
 def main():
-    thread_id = cgi.FieldStorage()
-    print t.substitute(thread_id = thread_id["thread"].value, form = form.substitute())
-t = Template("""Content-Type: text/html;charset=utf-8\n\n
+    form = cgi.FieldStorage()
+    thread_id = form.getvalue("thread", "none")
+    name = form.getvalue("name","none")
+    date = form.getvalue("date","none")
+    comment = form.getvalue("comment","none")
+    if form.has_key("name"):
+        insert_comment(thread_id, name, comment)
+
+    print html1.substitute(thread_id = thread_id)
+
+    show_comment(thread_id)
+
+    print html2.substitute(form = comment_box.substitute(thread_id = thread_id))
+
+html1 = Template("""Content-Type: text/html;charset=utf-8\n\n
 <!doctype html>
 <html>
 <title>thread${thread_id}</title>
@@ -38,25 +88,9 @@ t = Template("""Content-Type: text/html;charset=utf-8\n\n
 <div align=\"center\">
 <a href=\"thread.py\">戻る</a>
 <br><br>
-<table border=1>
-<tr><td width=\"580\">1 inoue 2015/6/25/9:46</td></tr>
-<tr><td>あいうえおかきくけこさしすせそたちつてとなにぬねのがぎふへほまみむめもやゆよらりるれろわをん</td></$
-</table>
-<br>
-<table border=1>
-<tr><td width=\"580\">2 inoue 2015/6/25/9:47</td></tr>
-<tr><td>アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン</td></$
-</table>
-<br>
-<table border=1>
-<tr><td width=\"580\">1 inoue 2015/6/25/9:46</td></tr>
-<tr><td>あいうえおかきくけこさしすせそたちつてとなにぬねのがぎふへほまみむめもやゆよらりるれろわをん</td></$
-</table>
-<br>
-<table border=1>
-<tr><td width=\"580\">2 inoue 2015/6/25/9:47</td></tr>
-<tr><td>アイウエオカキクケコサシスセソタチツテトナニヌネノハヒフヘホマミムメモヤユヨラリルレロワヲン</td></$
-</table>
+""")
+
+html2 = Template("""
 ${form}
 </div>
 </html>
@@ -65,14 +99,14 @@ ${form}
 comments = Template("""
 <br>
 <table border=1>
-<tr><td width=\"580\">${comment_id} ${name} ${date}</td></tr>
-<tr><td>${comment}</td></$
+<tr><td width=\"580\">${number} ${name} ${date}</td></tr>
+<tr><td>${comment}</td></tr>
 </table>
 """)
 
-form = Template("""
+comment_box = Template("""
 <br><br>
-<form action=\"http://ubuntu7/inoue/insert_comment.py\" method=\"post\">
+<form action=\"http://ubuntu7/inoue/comment.py?thread=${thread_id}\" method=\"post\">
 <table border=1><tr><td width=\"400\">
 <p>　名前：<input type=\"text\" name=\"name\" size=\"40\"></p>
 <p>　コメント<br><div align=\"center\"><textarea name=\"comment\" cols=\"40\" rows=\"10\" ></textarea></p>
@@ -81,5 +115,8 @@ form = Template("""
 </td></tr></table>
 </form>
 """)
+
+select_id = Template("""select * from test.comments where thread_id = ${thread_id};""")
+
 
 main()
--- thread.py
+++ thread.py
@@ -5,33 +5,108 @@ import cgi
 import cgitb
 import MySQLdb
 from string import Template
+import datetime
 cgitb.enable()
 
-def main():
-    print t.substitute()
+def show_thread():
+    connector = MySQLdb.connect(host = "localhost", db = "test", user = "inoue", passwd = "inoPWit", charset = "utf8")
+    cursor = connector.cursor()
 
-t=Template("""Content-Type: text/html;charset=utf-8\n\n
+    cursor.execute("select * from test.threads;")
+    result = cursor.fetchall()
+
+    for row in result:
+        print threads.substitute(thread_id = row[0], thread_name = row[1].encode("utf-8"), creator_name = row[2].encode("utf-8"), comment_number = str(row[3]), update_day = str(row[4]))
+
+threads = Template("""
+<tr><td><table><tr><td width=\"300\"><a href=\"comment.py?thread=${thread_id}\">${thread_name}</a></td><td width=\"200\">${creator_name}</td><td width=\"100\"><div align=\"center\">${comment_number}</div></td$
+""")
+
+def insert_thread(thread_name, creator_name):
+    connector = MySQLdb.connect(host = "localhost", db = "test", user = "inoue", passwd = "inoPWit", charset = "utf8")
+    cursor = connector.cursor()
+
+    cursor.execute("select thread_id from test.threads order by thread_id desc;")
+    result = cursor.fetchone()
+    if result == None:
+        thread_id = 1
+    else:
+        thread_id = result[0] + 1
+cursor.execute("select comment_id from test.comments order by comment_id desc;")
+    result = cursor.fetchone()
+    if result == None:
+        comment_id = 1
+    else:
+        comment_id = result[0] + 1
+
+    d = datetime.datetime.today()
+    date = str(d.year) + "/" + str(d.month) + "/" + str(d.day) + "/" + str(d.hour) + "/" + str(d.minute) + "/" + str(d.second)
+
+    sql1 = "insert into test.threads values(\"" + str(thread_id) +"\", \"" + thread_name + "\", \"" + creator_name + "\", \"1\", \"" + date + "\");"
+    cursor.execute(sql1)
+
+    sql2 = "insert into test.comments values(\"" + str(comment_id) + "\", \"" + str(thread_id) + "\", \"" + creator_name + "\", \"" + date + "\", \"新規スレッドを作成しました。\", \"false\");"
+    cursor.execute(sql2)
+    
+    connector.commit()
+
+    cursor.close()
+    connector.close()
+
+def main():
+    form = cgi.FieldStorage()
+    create_flag = form.getvalue("create_thread", "false")
+
+    if create_flag == "true":
+        print create_thread.substitute()
+    else:
+        print thread1.substitute()
+        show_thread()
+        print thread2.substitute()
+
+    thread_name = form.getvalue("thread_name")
+    creator_name = form.getvalue("creator_name")
+    if  form.has_key("thread_name"):
+        insert_thread(thread_name, creator_name)
+
+thread1 = Template("""Content-Type: text/html;charset=utf-8\n\n
 <!doctype html>
 <html>
 <title>thread</title>
 <font size=\"10\"><div align=\"center\">BBS</div></font>
 <div align=\"center\">
+<font><a href=\"thread.py?create_thread=true\">新規作成</a></font>
 <table border=1>
-<td valign=\"top\" width=\"583\" height=\"21\" bgcolor=\"#e8e8e8\">
-スレッド一覧
-</td>
-<tr>
-<td><a href=\"comment.py?thread=1\">スレッド１</a></td>
-</tr>
-<tr>
-<td><a href=\"comment.py?thread=2\">スレッド２</a></td>
-</tr>
+<tr><td bgcolor=\"#e8e8e8\" valign=\"top\" width=\"800\"><div align=\"center\">スレッド一覧</div></td></tr>
+<tr><td><table><tr><td width=\"300\">スレッド名<td><td width=\"200\">制作者</td><td width=\"100\"><div align=\"center\">コメント数</div></td><td width=\"200\"><div align=\"center\">最終更新時間</div></td></tr$
+""")
+
+thread2 = Template("""
 </table>
 </div>
+</html>
+""")
 
-
-<h>太郎</h>
-<p>押されたボタンは[]です</p>
+create_thread = Template("""Content-Type: text/html;charset=utf-8\n\n
+<!doctype html>
+<html>
+<title>create_thread</title>
+<br><br><br>
+<div align=\"center\">
+<font size=30>スレッド作成</font>
+<br><br>
+<p><a href=\"thread.py?create_thread=false\">戻る</a></p>
+<form action=\"http://ubuntu7/inoue/thread.py?create_thread=false\" method=\"post\">
+<table border=1><tr><td width=\"400\">
+<p>　　　　名前：<input type=\"text\" name=\"creator_name\" size=\"40\"></p>
+<p>　スレッド名：<input type=\"text\" name=\"thread_name\" size=\"40\"></p>
+<div align=\"center\">
+<input type=\"submit\" value=\"作成\">
+<input type=\"reset\" value=\"リセット\"></div><br>
+</div>
+</td></tr></table>
+</form>
+</div>
 </html>
 """)
 
